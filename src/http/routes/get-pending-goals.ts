6111486ed37e3036f35cb260c57c82f91b2a6f3a
Lucas Alves <alvez.lucas2702@gmail.com>
2024-09-12 14:53:22 -0300

    feat: finish backend

---
+++ src/http/routes/get-pending-goals.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod'
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import { createGoal } from '../../functions/create-goal'
+import { createGoalCompletion } from '../../functions/create-goal-completion'
+import { getWeekPendingGoals } from '../../functions/get-week-pending-goals'
+
+export const getPendingGoalsRoute: FastifyPluginAsyncZod = async app => {
+  app.get('/pending-goals', async () => {
+    const { pendingGoals } = await getWeekPendingGoals()
+
+    return { pendingGoals }
+  })
+}
